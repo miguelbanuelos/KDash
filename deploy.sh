cc26d5d135919c03ef22c6869aea92fd73e27388
Migue Bañuelos <kaiser.gdl@gmail.com>
2026-05-16 04:08:09 -0600

    Fix: Agregar safe.directory para evitar bloqueo de propiedad dudosa en Git

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /docker/KDash
+git config --global --add safe.directory /docker/KDash
 git fetch --all
 git reset --hard origin/main
 docker compose up -d --build --force-recreate dash-app
